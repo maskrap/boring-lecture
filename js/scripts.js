d9905a549775ae62730501f7cad070fdf40b35b4
Olivia Hinton and Sam Park <olivia@>
2016-05-31 16:23:39 -0700

    Add back to black buttON

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $("button#invert").click(function() {
+    $("body").removeClass();
     $("body").addClass("black-background");
   });
+
+  $("button#go-back").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+
+
 });
